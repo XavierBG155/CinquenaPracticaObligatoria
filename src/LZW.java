f17f04b60db8bc5a26b65108458fda68515d9b3d
Xavi Berga <xavierbg57.xbg@gmail.com>
2023-05-26 17:26:55 +0200

    un poc de lzw

--- src/LZW.java
+++ src/LZW.java
@@ -2,30 +2,65 @@
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class LZW {
     public static void compress(InputStream is, OutputStream os) {
         try {
-            int llarg = is.available();
-            Map<Character, Integer> diccionari = new HashMap<>();
-            for (int i = 0; i < llarg; i++) {
-                char c = (char) is.read();
-                for (int j = 0; j < 256; j++) {
-
-                    if (!diccionari.containsKey(c)) {
-                        diccionari.put(c, i);
-                    }
+            List<Byte> diccionari = new ArrayList<>();
+            byte[] arIs = is.readAllBytes();
+            int index = 0;
+            byte anterior;
+            for (int i = 0; i < arIs.length; i++) {
+                byte b = arIs[i];
+                index = search(b, diccionari, index);
+                if (index == 0) {
+                    diccionari.add(b);
+                    os.write(index);
+                    os.write(b);
+                    continue;
                 }
-                System.out.println(diccionari);
+
+                if (diccionari.get(index) == b){
+
+                }
             }
-        } catch (IOException e) {
+        } catch (
+                IOException e) {
             throw new RuntimeException(e);
         }
+
+    }
+
+    /*private static void passaOP(List<Byte> diccionari, OutputStream os) throws IOException {
+        for (byte b : diccionari) {
+            os.write(b);
+        }
+    }*/
+
+    private static int search(int c, List<Byte> dic, int index) {
+        for (byte b : dic) {
+            if (c == b) return index + 1;
+            index++;
+        }
+        return 0;
     }
 
     public static void decompress(InputStream is, OutputStream os) {
 
     }
+
+    /*static class tableEntry {
+        int index;
+        byte symbol;
+
+        tableEntry(int index, byte symbol) {
+            this.index = index;
+            this.symbol = symbol;
+        }
+    }*/
 }
+
